066943de201fd39acedbaf5bdf66be15f854c536
icecrimu <charles.banigon@gmail.com>
2022-02-06 07:30:36 +0800

    Component Breakdown and Recipe JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import RecipeList from "./RecipeList"
 function App() {
-  return null
+  return <RecipeList recipes={sampleRecipes} />
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: "Plain Chicken",
+    cookTime: "1:15",
+    servings: 3,
+    instructions:
+      "1. Put salt on chicken \n2. Put chicken in oven\n 3. Eat chicken"
+  },
+  {
+    id: 2,
+    name: "Plain Pork",
+    cookTime: "0:30",
+    servings: 1,
+    instructions: "1. Put salt on pork\n 2. Put pork in oven \n 3. Eat pork"
+  }
+]
 
 export default App
