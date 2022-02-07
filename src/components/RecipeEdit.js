437a56d92b8bd4536ab43586fb97a8c05f1a21a5
icecrimu <charles.banigon@gmail.com>
2022-02-07 08:39:52 +0800

    RecipeEdit CSS

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -4,29 +4,62 @@ import RecipeIngredientEdit from "./RecipeIngredientEdit"
 export default function RecipeEdit() {
   return (
     <div className="recipe-edit">
-      <div>
-        <button>&times;</button>
+      <div className="recipe-edit__remove-btn-container">
+        <button className="recipe-edit__remove-btn">&times;</button>
       </div>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="cookTime">Cook Time</label>
-        <input type="text" name="cookTime" id="cookTime" />
-        <label htmlFor="servings">Servings</label>
-        <input type="number" name="servings" id="servings" />
-        <label htmlFor="instructions">Instructions</label>
-        <textarea type="text" name="instructions" id="instructions" />
+      <div className="recipe-edit__details-grid">
+        <label htmlFor="name" className="recipe-edit__label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="recipe-edit__input"
+          name="name"
+          id="name"
+        />
+
+        <label htmlFor="cookTime" className="recipe-edit__label">
+          Cook Time
+        </label>
+        <input
+          type="text"
+          className="recipe-edit__input"
+          name="cookTime"
+          id="cookTime"
+        />
+
+        <label htmlFor="servings" className="recipe-edit__label">
+          Servings
+        </label>
+        <input
+          type="number"
+          className="recipe-edit__input"
+          name="servings"
+          min="1"
+          id="servings"
+        />
+
+        <label htmlFor="instructions" className="recipe-edit__label">
+          Instructions
+        </label>
+        <textarea
+          type="text"
+          className="recipe-edit__input"
+          name="instructions"
+          id="instructions"
+        />
       </div>
       <br />
-      <label>Ingredients</label>
-      <div>
+      <label className="recipe-edit__label">Ingredients</label>
+      <div className="recipe-edit__ingredient-grid">
         <div>Name</div>
         <div>Amount</div>
+        <div></div>
         <RecipeIngredientEdit />
         <RecipeIngredientEdit />
       </div>
-      <div>
-        <button>Add Ingredient</button>
+      <div className="recipe-edit__add-ingredient-btn-container">
+        <button className="btn btn--primary">Add Ingredient</button>
       </div>
     </div>
   )
--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function RecipeIngredientEdit() {
   return (
     <>
-      <input type="text" />
-      <input type="text" />
-      <button>&times;</button>
+      <input type="text" className="recipe-edit__input" />
+      <input type="text" className="recipe-edit__input" />
+      <button className="btn btn--danger">&times;</button>
     </>
   )
 }
